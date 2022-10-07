b9cdb84e2f67453fa5218813b73649e7841a58c4
Curran Duke <cdsonics@yahoo.com>
2022-10-06 20:47:10 -0400

    added comments and checked for typos

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,15 +1,17 @@
+
+// ALL QUESTIONS FOR QUIZ IN AN ARRAY
 var questions = [
-    {
+    { //question #1
         title: 'Commonly used data types DO NOT include:',
         choices: [
             'strings',
             'booleans',
             'alerts',
             'numbers'
-        ],
+        ], // answer #2
         answer: 'alerts'
     },
-    {
+    { // question #2
         title: 'The condition in an if / else statement is enclosed within ___.',
         choices: [
             'quotes',
@@ -17,19 +19,19 @@ var questions = [
             'parentheses',
             'square brackets'
         ],
-        answer: 'parentheses'
+        answer: 'parentheses' // answer #2 
     },
-    {
-        title: 'Arrays in JavaScript can be used to store ___.',
+    {   
+        title: 'Arrays in JavaScript can be used to store ___.', // question #3
         choices: [
             'numbers and strings',
             'other arrays',
             'booleans',
             'all of the above'
         ],
-        answer: 'all of the above'
+        answer: 'all of the above' // answer #3
     },
-    {
+    { // question #4
         title: 'String values must be enclosed within ___ when being assigned to variables.',
         choices: [
             'commas',
@@ -37,9 +39,9 @@ var questions = [
             'quotes',
             'parentheses'
         ],
-        answer: 'quotes'
+        answer: 'quotes' // answer #4
     },
-    {
+    { // question #5
         title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
         choices: [
             'JavaScript',
@@ -47,6 +49,6 @@ var questions = [
             'for loops',
             'console.log'
         ],
-        answer: 'console.log'
+        answer: 'console.log' // answer #5
     }
 ];
