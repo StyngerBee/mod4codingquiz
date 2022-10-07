7f7a69b950ff9eda6d1bdf83330fb79a619f37b8
Curran Duke <cdsonics@yahoo.com>
2022-10-06 20:50:24 -0400

    added comments

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,11 +1,11 @@
 function printHighscores() {
-  // either get scores from localstorage or set to empty array
+  // either get scores from local storage or set to empty array
   var scores = JSON.parse(localStorage.getItem("savedScores"));
   // sort highscores by score property in descending order
   scores.sort( function ( a, b ) { return b.score - a.score; } );
   // create li tag for each high score
   for(var i=0;i<scores.length;i++) {
-      //create new button for each choice
+      //create new button for each choice 
       var li = document.createElement("li");
       li.textContent = scores[i].init + " - " + scores[i].score;
       //display on the page
@@ -15,13 +15,13 @@ function printHighscores() {
 
 /* FUNCTION TO CLEAR SCORES */
 function clearHighscores() {
-  // remove an item from local storage
+  // removes an item from local storage
   localStorage.removeItem("savedScores");
-  // reload the page
+  // reloads the page
   window.location.reload();
 }
 
-/* CLICK EVENT TO RUN THE CLEAR SCORES FUNCTION */
+/* event to run the clear highscores function after being clicked */
 document.querySelector("#clear").addEventListener("click", clearHighscores);
-// run function when page loads
+// runs the  function after the page loads
 printHighscores();
