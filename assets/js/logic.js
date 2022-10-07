b06d6f2434477ff65721d420e7cd5073d4aa9852
Curran Duke <cdsonics@yahoo.com>
2022-10-06 20:20:47 -0400

    downloaded all code from personal laptop(offline) and pasted into here for repo

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,104 +1,158 @@
 // variables to keep track of quiz state
-    // currentQuestion
-    // time
-    // timerId
+var currentQuestion = 0;
+var time = 60;
+var timer;
+
 
 // variables to reference DOM elements
 var questionsEl = document.getElementById('questions');
 
 
-/// FUNCTION TO START THE QUIZ
-function startQuiz() {
-  // hide start screen 
-  
-  // un-hide questions section
+/* FUNCTION TO START THE QUIZ */
+function startQuiz(event) {
+event.stopPropagation();
+// hide start screen
+document.getElementById('start-screen').className ="hide";
 
-  // start timer
+// un-hide questions section
+document.getElementById('questions').className ="show";
+// start timer
+clockTick();
 
-  // show starting time
-
-  getQuestion();
+getQuestion();
 }
 
-/// FUNCTION TO GET/SHOW EACH QUESTION ///
+/* FUNCTION TO GET/SHOW EACH QUESTION */
 function getQuestion() {
-  // get current question object from array
+// get current question object from array
+var questionValue = questions[currentQuestion];
 
-  // update title with current question
+// update title with current question
 
-  // clear out any old question choices
+document.getElementById('question-title').textContent =  questionValue.title;
 
-  // loop over choices
-    // FOR {
-      // create new button for each choice
-  
-      // display on the page
-      
-    // } 
+// clear out ant old question choices
+document.getElementById('choices').innerHTML = '';
+
+// loop over choices
+for(var i=0;i<questionValue.choices.length;i++) {
+    //create new button for each choice
+    var btn = document.createElement("button");
+    btn.textContent = i+1 + ". " +questionValue.choices[i];
+    btn.setAttribute("data-index", i);
+    //display on the page
+    document.querySelector("#choices").appendChild(btn);
+}
 }
 
-/// FUNCTION FOR CLICKING A QUESTION ///
+/* FUNCTION FOR CLICKING A QUESTION */
 function questionClick(event) {
-
-  // if the clicked element is not a choice button, do nothing.
-  if (something) {
-
-  }
-
-  if (something) {
-  // check if user guessed wrong
-    // penalize time
-
-    // display new time on page
-
-    // give them feedback, letting them know it's wrong
-  } else {
-    // give them feedback, letting them know it's right
-  }
-
-  // flash right/wrong feedback on page for a short period of time
-
-  // move to next question
-
-  // check if we've run out of questions
-    // if so, end the quiz
-    // else, get the next question
+//event.stopPropagation();
+var element = event.target;
+// if the clicked element is not a choice button, do nothing.
+if (element.matches("button") !== true) {
+}
+else{
+    var questionValue = questions[currentQuestion];
+    // check if user guessed wrong
+    if(questionValue.answer !== questionValue.choices[element.getAttribute("data-index")]){
+        // penalize time
+        if(time>10){
+            time -= 10;
+            // display new time on page
+            document.getElementById("timeText").textContent = "Time: " + time;
+        }
+        else{
+            time-=time;
+            // display new time on page
+            document.getElementById("timeText").textContent = "Time: " + time;
+            quizEnd();
+        }
+        // give them feedback, letting them know it's wrong
+        document.getElementById("feedback").textContent = "Incorrect!";
+        document.getElementById("feedback").className = "show";
+    }
+    else{
+        console.log("You guessed right!");
+        // give them feedback, letting them know it's right
+        document.getElementById("feedback").textContent = "Correct!";
+        document.getElementById("feedback").className = "show";
+        // move to next question
+        currentQuestion++;
+        if(currentQuestion>3){
+            quizEnd()
+        }
+        else{
+            getQuestion();
+        }
+    }
+    
+}
 }
 
-/// FUNCTION TO END THE QUIZ ///
+/* FUNCTION TO END THE QUIZ */
 function quizEnd() {
-  // stop timer
-
-  // show end screen
-
-  // show final score
-
-  // hide questions section
+// stop timer
+clearInterval(timer);
+// show end screen
+document.getElementById('end-screen').className ="show";
+// show final score
+document.getElementById("final-score").textContent = time;
+// hide questions section
+document.getElementById('questions').className ="hide";
+document.getElementById("feedback").className = "hide";
 }
 
-/// FUNCTION FOR UPDATING THE TIME ///
+/* FUNCTION FOR UPDATING THE TIME */
 function clockTick() {
-  // update time
+ timer = setInterval(function() {
+    
+    time--;
+    // update time
+    document.getElementById("timeText").textContent = "Time: " + time;
 
-  // check if user ran out of time
+    if (time === 0) {
+        // Clears interval
+        clearInterval(timer);
+        quizEnd();
+    }
+  }, 1000);
+
 }
 
 function saveHighscore() {
-  // get value of input box - for initials
-
-  // make sure value wasn't empty
-    // get saved scores from localstorage, or if not any, set to empty array
-
-    // format new score object for current user
-
-    // save to localstorage
-
-    // redirect to next page
+// get value of input box - for initials
+var initials = document.getElementById('initials').value;
+// format new score object for current user
+var currentScore = {init: initials, score: time};
+// get saved scores from localstorage, or if not any, set to empty array
+var savedScores = JSON.parse(localStorage.getItem("savedScores"));
+    // make sure value wasn't empty
+if(initials === ""){
+    alert ("You must enter your initials to save your score");
 }
 
-/// CLICK EVENTS ///
-  // user clicks button to submit initials
+else{
+    if (savedScores !== null) {
+        savedScores.push(currentScore);
+        // save to local storage
+        localStorage.setItem("savedScores",JSON.stringify(savedScores));
+    }
+    else{
+        savedScores = [currentScore];
+        localStorage.setItem("savedScores", JSON.stringify(savedScores));
+    }
+        // redirect to next page
+        document.location.href = "./highscores.html";
+}
+}
 
-  // user clicks button to start quiz
+/* CLICK EVENTS */
+// user clicks button to submit initials
+document.querySelector("#submit").addEventListener("click", saveHighscore);
 
-  // user clicks on element containing choices
+// user clicks button to start quiz
+document.querySelector("#start-quiz").addEventListener("click", startQuiz);
+
+// user clicks on element containing choices
+document.querySelector("#questions").addEventListener("click", questionClick);
